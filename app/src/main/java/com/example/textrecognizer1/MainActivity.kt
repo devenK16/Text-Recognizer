51fe6d9fb6988b78a191640ab9fa341a5098e251
devenK16 <devenkhobragade007@gmail.com>
2023-07-15 16:00:22 +0530

    initial commit

---
+++ app/src/main/java/com/example/textrecognizer1/MainActivity.kt
@@ -0,0 +1,351 @@
+package com.example.textrecognizer
+
+import android.Manifest
+import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.text.util.Linkify
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.example.textrecognizer1.databinding.ActivityMainBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var recognizer: TextRecognizer
+    private val TAG = "Testing"
+    private val SAVED_TEXT_TAG = "SavedText"
+
+    //private val SAVED_IMAGE_BITMAP = "SavedImage"
+    private val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+    lateinit var camera: Camera
+    var savedBitmap: Bitmap? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        if (savedInstanceState != null) {
+            val savedText = savedInstanceState.getString(SAVED_TEXT_TAG)
+            binding.apply {
+                if (isTextValid(savedText)) {
+                    textInImageLayout.visibility = View.VISIBLE
+                    textInImage.text = savedInstanceState.getString(SAVED_TEXT_TAG)
+                }
+                if (savedBitmap != null) {
+                    previewImage.visibility = View.VISIBLE
+                    previewImage.setImageBitmap(savedBitmap)
+                }
+                //previewImage.setImageBitmap(savedInstanceState.getParcelable(SAVED_IMAGE_BITMAP))
+            }
+        }
+        init()
+        setContentView(binding.root)
+    }
+
+    private fun init() {
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        recognizer = TextRecognition.getClient()
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions()
+        }
+
+        binding.apply {
+            extractTextButton.setOnClickListener {
+                when {
+                    previewImage.visibility == View.VISIBLE -> {
+                        savedBitmap = previewImage.drawable.toBitmap()
+                        runTextRecognition(savedBitmap!!)
+                    }
+                    viewFinder.bitmap != null -> {
+                        previewImage.visibility = View.VISIBLE
+                        savedBitmap = viewFinder.bitmap
+                        previewImage.setImageBitmap(viewFinder.bitmap!!)
+                        runTextRecognition(savedBitmap!!)
+                    }
+                    else -> {
+                        showToast(getString(R.string.camera_error_default_msg))
+                    }
+                }
+            }
+
+
+
+            copyToClipboard.setOnClickListener {
+                val textToCopy = textInImage.text
+                if (isTextValid(textToCopy.toString())) {
+                    copyToClipboard(textToCopy)
+                } else {
+                    showToast(getString(R.string.no_text_found))
+                }
+            }
+
+            share.setOnClickListener {
+                val textToCopy = textInImage.text.toString()
+                if (isTextValid(textToCopy)) {
+                    shareText(textToCopy)
+                } else {
+                    showToast(getString(R.string.no_text_found))
+                }
+            }
+
+            close.setOnClickListener {
+                textInImageLayout.visibility = View.GONE
+            }
+
+        }
+
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+        )
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                showToast(
+                    getString(R.string.permission_denied_msg)
+                )
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.gallery) {
+            binding.textInImageLayout.visibility = View.GONE
+            ImagePicker.with(this)
+                .galleryOnly()
+                .crop()
+                .compress(1024)
+                .maxResultSize(1080, 1080)
+                .start()
+
+            return true
+        } else if (item.itemId == R.id.camera) {
+            if(!allPermissionsGranted()){
+                requestPermissions()
+            } else {
+                binding.previewImage.visibility = View.GONE
+                savedBitmap = null
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                //Image Uri will not be null for RESULT_OK
+                val uri: Uri = data?.data!!
+
+                // Use Uri object instead of File to avoid storage permissions
+                binding.previewImage.apply {
+                    visibility = View.VISIBLE
+                    setImageURI(uri)
+                }
+                //runTextRecognition(binding.previewImage.drawable.toBitmap())
+            }
+            ImagePicker.RESULT_ERROR -> {
+                showToast(ImagePicker.getError(data))
+            }
+            else -> {
+                showToast("No Image Selected")
+            }
+        }
+    }
+
+    private fun runTextRecognition(bitmap: Bitmap) {
+        val inputImage = InputImage.fromBitmap(bitmap, 0)
+
+        recognizer
+            .process(inputImage)
+            .addOnSuccessListener { text ->
+                binding.textInImageLayout.visibility = View.VISIBLE
+                processTextRecognitionResult(text)
+            }.addOnFailureListener { e ->
+                e.printStackTrace()
+                showToast(e.localizedMessage ?: getString(R.string.error_default_msg))
+            }
+    }
+
+    private fun processTextRecognitionResult(result: Text) {
+        var finalText = ""
+        for (block in result.textBlocks) {
+            for (line in block.lines) {
+                finalText += line.text + " \n"
+            }
+            finalText += "\n"
+        }
+
+        Log.d(TAG, finalText)
+        Log.d(TAG, result.text)
+
+        binding.textInImage.text = if (finalText.isNotEmpty()) {
+            finalText
+        } else {
+            getString(R.string.no_text_found)
+        }
+
+        Linkify.addLinks(binding.textInImage, Linkify.ALL)
+
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+
+
+                binding.apply {
+
+                    camera.apply {
+                        torchImage.setBackgroundColor(resources.getColor(R.color.purple_200))
+                        if (cameraInfo.hasFlashUnit()) {
+                            torchButton.setOnClickListener {
+                                cameraControl.enableTorch(cameraInfo.torchState.value == TorchState.OFF)
+                            }
+                        } else {
+                            torchButton.setOnClickListener {
+                                showToast(getString(R.string.torch_not_available_msg))
+                            }
+                        }
+
+                        cameraInfo.torchState.observe(this@MainActivity) { torchState ->
+                            if (torchState == TorchState.OFF) {
+                                torchImage.setImageResource(R.drawable.ic_flashlight_on)
+                            } else {
+                                torchImage.setImageResource(R.drawable.ic_flashlight_off)
+                            }
+                        }
+
+                    }
+
+                }
+            } catch (exc: Exception) {
+                showToast(getString(R.string.error_default_msg))
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isTextValid(text: String?): Boolean {
+        if (text == null)
+            return false
+
+        return text.isNotEmpty() and !text.equals(getString(R.string.no_text_found))
+    }
+
+    private fun shareText(text: String) {
+        val shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text)
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share_text_title)))
+    }
+
+    private fun copyToClipboard(text: CharSequence) {
+        val clipboard =
+            ContextCompat.getSystemService(applicationContext, ClipboardManager::class.java)
+        val clip = ClipData.newPlainText("label", text)
+        clipboard?.setPrimaryClip(clip)
+        showToast(getString(R.string.clipboard_text))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.clear()
+        val textInImage = (binding.textInImage.text).toString()
+        if (isTextValid(textInImage)) {
+            outState.putString(SAVED_TEXT_TAG, textInImage)
+        }
+        /*if (binding.previewImage.visibility == View.VISIBLE) {
+            outState.putParcelable(SAVED_IMAGE_BITMAP, binding.previewImage.drawable.toBitmap())
+        }*/
+    }
+
+}
